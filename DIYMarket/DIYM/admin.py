91ff3f28fbde00a978a2cadd5d255cf476ce4dda
rocknroll7 <rolandodh@yahoo.com>
2015-05-02 12:07:35 -0500

    Address

--- DIYMarket/DIYM/admin.py
+++ DIYMarket/DIYM/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from blog.models import Post
+
+admin.site.register(Post)
+admin.site.register(Address)
+admin.site.register(DIYUser)
+admin.site.register(ItemService)
+admin.site.register(Offer)
